856467554a81c36051fd9191e431c80d3a15a181
Sotono <sotono.dk@gmail.com>
2020-11-03 03:26:11 +0900

    add  requiments

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 # Flaskとrender_template（HTMLを表示させるための関数）をインポート
 from flask import Flask, render_template, request
 import re
-from models import convert
+# from models import convert
 
 
 # Flaskオブジェクトの生成
@@ -14,11 +14,15 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     name = request.args.get('before')
-    return render_template('index.html', name=name)
+    return render_template('index.html')
 
 
 @app.route('/', methods=['post'])
 def post():
     body = request.form['before']
-    body = convert.convert(body)
+    # body = convert.convert(body)
     return render_template('index.html', body=body)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
